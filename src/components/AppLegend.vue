36d9ae720737b11aa23d25400d7898a1ef6e1208
index-daddy372466 <testmailbox987654@gmail.com>
2025-09-15 18:36:44 -0400

    chore: update app legend UI

--- src/components/AppLegend.vue
+++ src/components/AppLegend.vue
@@ -11,14 +11,14 @@
 #legend-container{
 transition: .25s;
 position:fixed;
-right:45px;
-top:33%;
+right:20px;
+bottom:175px;
 border-radius: 12px;
 /* bordeR:2px solid green; */
 border-right:1px solid hsla(160, 100%, 37%, 1);
 border-left:1px solid hsla(160, 100%, 37%, 1);
 width:35px;
-height:50%;
+height: 350px;
 display:flex;
 flex-direction: column;
 align-items: center;
